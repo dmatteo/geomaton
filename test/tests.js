f8688f72f2b45aa5738dfb4fc2dba0aad1e57fea
Domenico Matteo <matteo.domenico@gmail.com>
2015-06-16 15:45:26 +0200

    add `unexpected-mitm` interceptor to mock out Google API responses

--- test/tests.js
+++ test/tests.js
@@ -1,91 +1,27 @@
-
-var results = [
-  {
-    "address_components" : [
-      {
-        "long_name" : "2",
-        "short_name" : "2",
-        "types" : [ "street_number" ]
-      },
-      {
-        "long_name" : "Market Street",
-        "short_name" : "Market St",
-        "types" : [ "route" ]
-      },
-      {
-        "long_name" : "San Francisco",
-        "short_name" : "SF",
-        "types" : [ "locality", "political" ]
-      },
-      {
-        "long_name" : "San Francisco County",
-        "short_name" : "San Francisco County",
-        "types" : [ "administrative_area_level_2", "political" ]
-      },
-      {
-        "long_name" : "California",
-        "short_name" : "CA",
-        "types" : [ "administrative_area_level_1", "political" ]
-      },
-      {
-        "long_name" : "United States",
-        "short_name" : "US",
-        "types" : [ "country", "political" ]
-      },
-      {
-        "long_name" : "94105",
-        "short_name" : "94105",
-        "types" : [ "postal_code" ]
-      }
-    ],
-    "formatted_address" : "2 Market Street, San Francisco, CA 94105, USA",
-    "geometry" : {
-      "bounds" : {
-        "northeast" : {
-          "lat" : 37.7944795,
-          "lng" : -122.3948819
-        },
-        "southwest" : {
-          "lat" : 37.7944695,
-          "lng" : -122.3948952
-        }
-      },
-      "location" : {
-        "lat" : 37.7944795,
-        "lng" : -122.3948952
-      },
-      "location_type" : "RANGE_INTERPOLATED",
-      "viewport" : {
-        "northeast" : {
-          "lat" : 37.7958234802915,
-          "lng" : -122.3935395697085
-        },
-        "southwest" : {
-          "lat" : 37.7931255197085,
-          "lng" : -122.3962375302915
-        }
-      }
-    },
-    "place_id" : "Ei0yIE1hcmtldCBTdHJlZXQsIFNhbiBGcmFuY2lzY28sIENBIDk0MTA1LCBVU0E",
-    "types" : [ "street_address" ]
-  }
-];
-
 var geomaton = require('../lib/geomaton');
+
 var expect = require('unexpected').clone();
 expect.installPlugin(require('unexpected-mitm'));
+expect.installPlugin(require('unexpected-http'));
+
+expect.addAssertion('to be parsed as', function (expect, subject, value) {
+  return expect('https://maps.googleapis.com/maps/api/geocode/json?address=' + subject, 'to yield response',
+    { body: expect.it('when passed as parameter to', geomaton.parse, 'to equal', value) });
+});
 
 describe('geomaton.js', function() {
 
-  it('should output something', function() {
-    var parsed = geomaton.parse(results);
-    expect(parsed, 'to satisfy', {
-      state: 'California',
-      country: 'United States',
-      city: 'San Francisco',
-      zip: '94105'
-    });
+  // copy/paste this assertion to create mocked out responses
+  it.skip('Usage example of `with http recorded and injected`', function() {
+    var address = 'via Milano 25, Cologno Monzese';
+    var parsed =   {
+      state: 'Lombardia',
+      country: 'Italy',
+      city: 'Cologno Monzese',
+      zip: '20093',
+      address: 'Via Milano 25'
+    };
+    return expect(address, 'with http recorded and injected', 'to be parsed as', parsed);
   });
 
-  it('should config')
 });
